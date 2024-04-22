a8a92df1842d6a91169aa4cab02f30d8625c1c4d
Shezan <63561954+shezann@users.noreply.github.com>
2024-04-22 03:12:42 -0400

    convert to styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
-import Circle from "./components/Circle";
 import { motion } from "framer-motion";
+import styled from "styled-components";
+import Circle from "./components/Circle";
 import { PERSONAL_STATEMENT, circleTitles } from "./utils/constants";
-import "./App.css";
 
-// Assuming circleTitles is an array of strings
 const containerVariants = {
   hidden: {},
   visible: {
@@ -22,31 +21,62 @@ const App: React.FC = () => {
   };
 
   return (
-    <div className="container">
-      <div className="text-side">
+    <Container>
+      <TextSide>
         <h1>shezan mahmud</h1>
         <p>{PERSONAL_STATEMENT}</p>
-        {/* ...Other text and links */}
-      </div>
-      <div className="circle-side">
-        <motion.div
-          className="circle-container"
+        {/* Other text and links */}
+      </TextSide>
+      <CircleSide>
+        <StyledCircleContainer
           variants={containerVariants}
           initial="hidden"
           animate="visible"
         >
-          {circleTitles.map((title: string) => (
+          {circleTitles.map((title: string, index) => (
             <Circle
               key={title}
               text={title}
               isSelected={selected === title}
               onClick={() => handleCircleClick(title)}
+              index={index} // Pass index for position
             />
           ))}
-        </motion.div>
-      </div>
-    </div>
+        </StyledCircleContainer>
+      </CircleSide>
+    </Container>
   );
 };
 
 export default App;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  min-height: 100vh;
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+  text-align: left;
+`;
+
+const TextSide = styled.div`
+  flex: 1;
+  padding-right: 2rem;
+  color: black;
+`;
+
+const CircleSide = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledCircleContainer = styled(motion.div)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1em;
+`;
--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -1,11 +1,12 @@
 import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
-import "./Circle.css";
+import styled from "styled-components";
 
 interface CircleProps {
   isSelected?: boolean;
   text?: string;
   onClick?: () => void;
+  index: number;
 }
 
 const circleVariants = {
@@ -15,13 +16,8 @@ const circleVariants = {
     scale: 1.05,
     transition: { type: "spring", stiffness: 300 },
   },
-  hover_selected: {
-    scale: 1.3,
-    transition: { type: "spring", stiffness: 300 },
-  },
   selected: {
-    scale: 1.4,
-    backgroundColor: "#070750",
+    scale: 1,
     transition: { type: "spring", stiffness: 120 },
   },
 };
@@ -30,6 +26,7 @@ const Circle: React.FC<CircleProps> = ({
   isSelected = false,
   text = "",
   onClick,
+  index,
 }) => {
   const ref = useRef<HTMLDivElement>(null);
   const [position, setPosition] = useState({ x: 0, y: 0 });
@@ -40,7 +37,7 @@ const Circle: React.FC<CircleProps> = ({
       const { height, width, left, top } = ref.current.getBoundingClientRect();
       const middleX = clientX - (left + width / 2);
       const middleY = clientY - (top + height / 2);
-      setPosition({ x: middleX * 0.1, y: middleY * 0.1 });
+      setPosition({ x: middleX * 0.3, y: middleY * 0.3 });
     }
   };
 
@@ -49,22 +46,81 @@ const Circle: React.FC<CircleProps> = ({
   };
 
   return (
-    <motion.div
-      className={isSelected ? "selected_circle" : "circle"}
+    <StyledCircle
+      isSelected={isSelected}
       initial="hidden"
       animate={isSelected ? "selected" : "visible"}
-      whileHover={isSelected ? "hover_selected" : "hover"}
+      whileHover={isSelected ? "selected" : "hover"}
       variants={circleVariants}
       ref={ref}
       onMouseMove={handleMouse}
       onMouseLeave={reset}
       style={{ x: position.x, y: position.y, position: "relative" }}
       onClick={onClick}
+      index={index}
     >
-      <div className={isSelected ? "selected_glow" : "circle_glow"}></div>
-      {text && <div className="circle_text">{text}</div>}{" "}
-    </motion.div>
+      <CircleGlow />
+      {text && <CircleText>{text}</CircleText>}
+    </StyledCircle>
   );
 };
 
 export default Circle;
+
+const getPositionStyles = (index: number) => {
+  switch (index) {
+    case 0:
+      return "top: 0; left: 0%; transform: translate(-50%, -100%);";
+    case 1:
+      return "top: -35px; right: 70%; transform: translate(100%, -50%);";
+    case 2:
+      return "bottom: 70px; left: -40%; transform: translate(-50%, 100%);";
+    case 3:
+      return "top: -140px; left: 25%; transform: translate(-100%, -50%);";
+    default:
+      return "";
+  }
+};
+
+const StyledCircle = styled(motion.div)<{ isSelected: boolean; index: number }>`
+  width: ${({ isSelected }) => (isSelected ? "250px" : "190px")};
+  height: ${({ isSelected }) => (isSelected ? "250px" : "190px")};
+  border-radius: 50%;
+  background: ${({ isSelected }) => (isSelected ? "#070750" : "#2F2CD1")};
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-size: 20px;
+  overflow: hidden;
+  cursor: pointer;
+  transition:
+    background-color 0.3s,
+    width 0.3s,
+    height 0.3s,
+    transform 0.3s;
+  ${({ index }) => getPositionStyles(index)};
+`;
+
+const CircleText = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  padding: 0 1rem;
+  transition: color 0.3s;
+`;
+
+const CircleGlow = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  background: linear-gradient(to bottom right, transparent, white);
+  opacity: 0.5;
+  position: absolute;
+  top: 0;
+  left: 0;
+  transition: opacity 0.3s;
+`;
