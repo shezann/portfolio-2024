463384b45746eca19b414e55db139f348a2e38bd
Shezan <63561954+shezann@users.noreply.github.com>
2024-09-16 16:27:54 -0400

    update about text

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,4 +6,4 @@ export const circleTitles: string[] = [
 ];
 
 export const PERSONAL_STATEMENT: string =
-  "i’m a software engineer with experience in design and coding across various languages. i enjoy full-stack development, combining creativity with technical skills to build projects. i focus on the details because it’s the little things that make software great to use";
+  "software engineer with experience in design and coding across various languages. i enjoy full-stack development, combining creativity with technical skills to build projects. i love focusing on the details because it’s the little things that make software great to use";
